3532c1e11c8288b6c42a2f3ad4e8db0fad4fb4a3
Nils Wisiol <info@nils-wisiol.de>
2017-04-14 15:24:34 +0200

    fix(): hook waits until challenge published
    And some minor README improvements.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ To obtain a certificate for your dedyn.io domain right away, follow these steps.
 1. **Install certbot.** There are many ways to install certbot, depending on your distribution
 and preference. Please follow instructions on https://certbot.eff.org/
 
-2. **Install hook script.** To use authenticate your dedyn.io domain against Let's Encrypt
+2. **Install hook script.** To authenticate your dedyn.io domain against Let's Encrypt
 using the DNS challenge mechanism, you will need to update your domain according to
 instructions provided by Let's Encrypt. The hook script in this repository automatizes this
 process for you. To use it, download the `hook.sh` and `.dedynauth` files and place them
@@ -27,14 +27,15 @@ file to look something like
 dedyn hook scripts you just downloaded. For detailed instructions on how to use certbot,
 please refer to the certbot manual. A typical use of certbot is listed below. Please notice
 that you need to insert your domain name one more time. (Also, for users not familiar with bash,
-please not that you need to remove the `\` if you reformat the command to fit one line.) Depending
+please note that you need to remove the `\` if you reformat the command to fit one line.) Depending
 on how you installed certbot, you may need to replace `./certbot-auto` with just `certbot-auto`.
+Please also note that the hook script may wait up to two minutes to be sure that the challenge
+was correctly published.
 
          ./certbot-auto --manual \
          --text \
          --preferred-challenges dns \
          --manual-auth-hook ./hook.sh \
-         --manual-cleanup-hook ./hook.sh \
          -d "YOURDOMAINNAME.dedyn.io" \
          certonly
 
--- hook.sh
+++ hook.sh
@@ -6,7 +6,7 @@
 (
 if [ ! -f .dedynauth ]; then
     PATH=`pwd`/.dedynauth
-    echo "File $PATH not found. Please place .dedynauth file in appropriate location."
+    >&2 echo "File $PATH not found. Please place .dedynauth file in appropriate location."
     exit 1
 fi
 
@@ -14,34 +14,58 @@ source .dedynauth
 
 if [ -z "$DEDYN_TOKEN" ] ; then
     PATH=`pwd`./dedynauth
-    echo "Variable \$DEDYN_TOKEN not found. Please set DEDYN_TOKEN=(your dedyn.io token) to your dedyn.io access token in $PATH, e.g."
-    echo 
-    echo "DEDYN_TOKEN=d41d8cd98f00b204e9800998ecf8427e"
+    >&2 echo "Variable \$DEDYN_TOKEN not found. Please set DEDYN_TOKEN=(your dedyn.io token) to your dedyn.io access token in $PATH, e.g."
+    >&2 echo 
+    >&2 echo "DEDYN_TOKEN=d41d8cd98f00b204e9800998ecf8427e"
     exit 2
 fi
 
 if [ -z "$DEDYN_NAME" ] ; then
     PATH=`pwd`./dedynauth
-    echo "Variable \$DEDYN_NAME not found. Please set DEDYN_NAME=(your dedyn.io name) to your dedyn.io name in $PATH, e.g."
-    echo
-    echo "DEDYN_NAME=foobar.dedyn.io"
+    >&2 echo "Variable \$DEDYN_NAME not found. Please set DEDYN_NAME=(your dedyn.io name) to your dedyn.io name in $PATH, e.g."
+    >&2 echo
+    >&2 echo "DEDYN_NAME=foobar.dedyn.io"
     exit 3
 fi
 
 if [[ ! $(type -P curl) ]] ; then
-    echo "Please install curl to use certbot with dedyn.io."
+    >&2 echo "Please install curl to use certbot with dedyn.io."
     exit 4
 fi
+
+>&2 echo "Setting challenge to ${CERTBOT_VALIDATION}..."
 
 TOKEN=$CERTBOT_VALIDATION
 
 # be silent unless error, server response will still be written to output
 curl -X PATCH https://desec.io/api/v1/domains/$DEDYN_NAME/ \
-    -S \
+    -Ss \
     -H 'Accept: application/json' \
     -H "Authorization: Token $DEDYN_TOKEN" \
     -H 'Content-Type: application/json' \
-    -d "{\"acme_challenge\":\"$TOKEN\"}"
+    -d "{\"acme_challenge\":\"$TOKEN\"}" \
+    > /dev/null
+
+>&2 echo "Verifying challenge is set correctly. This can take up to 2 Minutes."
+>&2 echo "Current Time: `date`"
+
+for i in `seq 1 60`;
+do
+
+	CURRENT=$(host -t TXT _acme-challenge.$DEDYN_NAME ns1.desec.io | grep $TOKEN)
+	if [ ! -z "$CURRENT" ]; then
+		break
+	fi
+	sleep 2
+
+done
+
+if [ -z "$CURRENT" ]; then
+	>&2 echo "Token could not be published. Please check your dedyn credentials."
+	exit 5
+fi
+
+>&2 echo "Token published. Returning to certbot."
 
 )
 
